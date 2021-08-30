a599aff17f63d863c41d917afc18b1c8508b9e85
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-08-30 10:59:00 +0300

    Navigation to back and forth works

--- App.js
+++ App.js
@@ -16,10 +16,16 @@ const HomeScreen = ({ navigation }) => {
   );
 };
 
-const DetailsScreen = () => {
+const DetailsScreen = ({ navigation }) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
